44cdb58c3464b35a78e61a2f9cc5046df19ea2d4
SoKeY <sch-midi@windowslive.com>
2021-02-14 10:03:48 +0100

    Add mail icon

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,15 +1,20 @@
 import React from "react";
 import "./sidebar.css";
 import Avatar from "./avatar";
+import mailIcon from "../images/email-white-24dp.svg";
 
 const Sidebar = () => {
     return (
         <div className="sidebar fixed p-10 text-right">
             <Avatar />
             <div className="mt-10">
-            <p>Hey, Ich bin <strong>Yasmin Abdullahi</strong></p>
-            <a className="text-white" href="mailto:yasmin.abdullahi@gmail.com">yasmin.abdullahi@gmail.com</a>
-            <p>JGLP</p>
+                <p className="m-0">Yasmin Abdullahi</p>
+                <a className="text-white" href="mailto:abdullahi.ya@gmail.com">abdullahi.ya@gmail.com</a>
+                <div className="flex justify-end mt-6">
+                    <a href="mailto:abdullahi.ya@gmail.com">
+                        <img src={mailIcon}></img>
+                    </a>
+                </div>
             </div>
         </div>
     );
